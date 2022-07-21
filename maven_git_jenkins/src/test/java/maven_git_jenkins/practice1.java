22c81425fd3920a3e1470d5d6d15d9b951f1fa86
Sayantan Sinha <sinhashaan89@gmail.com>
2022-07-21 14:24:27 +0530

    commit1`

---
+++ maven_git_jenkins/src/test/java/maven_git_jenkins/practice1.java
@@ -0,0 +1,27 @@
+package maven_git_jenkins;
+
+import org.testng.annotations.Test;
+
+public class practice1 {
+@Test
+public void Practice25(){
+	System.out.println("This is method of maven for practice1,Practice1");
+}
+@Test
+public void Practice27(){
+	System.out.println("This is method of maven for practice1,Practice2");
+}
+@Test
+public void Practice36(){
+	System.out.println("This is method of maven for practice1,Practice3");
+}
+@Test
+public void Practice31(){
+	System.out.println("This is method of maven for practice1,Practice3");
+}
+	@Test
+public void Practice38(){
+	System.out.println("This is method of maven for practice1,Practice3");
+}
+	
+}
